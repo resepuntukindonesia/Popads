b09cca3bc13fd4d5758a3f3402e08b0cbf927870
resepuntukindonesia <resepuntukindonesia@gmail.com>
2015-10-17 08:49:42 +0700

    Update kanan.js

--- kanan.js
+++ kanan.js
@@ -1,7 +1,1 @@
-document.write("<script type="text/javascript">
-var var1 = "300";
-var var2 = "250";
-var var3 = "300x250";
-var var4 = "24179";
-var var5 = "e17d574a9ac6743fda8847722a2ce8c3";
-</script><script type="text/javascript" src="//cdn.adshexa.com/show_ads.php"></script>");
+document.write("<script type="text/javascript"> var var1 = "300"var var2 = "250"var var3 = "300x250" var var4 = "24179" var var5 = "e17d574a9ac6743fda8847722a2ce8c3" </script><script type="text/javascript" src="//cdn.adshexa.com/show_ads.php"></script>");
