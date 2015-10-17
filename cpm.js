6a77224baf24a5308f1372aef0d1ea7e9d561105
resepuntukindonesia <resepuntukindonesia@gmail.com>
2015-10-17 07:47:55 +0700

    Update cpm.js

--- cpm.js
+++ cpm.js
@@ -1,1 +1,15 @@
-document.write("<script type="text/javascript" src="//go.oclasrv.com/apu.php?zoneid=423486"></script>");
+<!--- START OF CPX24 CODE --->
+<script type=text/javascript>
+var _cpp= _cpp || [];
+_cpp['username']	= '';
+_cpp['source_id']	= '10126';
+_cpp['pop_type']	= '2';
+_cpp['onePer']  	= '0';
+_cpp['freq']		= '0';   
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('http://cdn1.adcdnx.com/adp1v2.js');
+var cs = document.getElementsByTagName('script')[0];
+cs.parentNode.insertBefore(hs, cs);
+})();</script>
+<!--- END OF CPX24 CODE --->
